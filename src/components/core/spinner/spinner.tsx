bec53925ed058adbd6f0107061947268a106898f
Gabriel Fernandes <bielfer18@gmail.com>
2025-01-23 16:53:16 -0300

    feat: spinner component

---
+++ src/components/core/spinner/spinner.tsx
@@ -0,0 +1,83 @@
+import type { FC } from 'react';
+
+import './styles.css';
+
+import { cn } from '~/lib/utils';
+
+type Size = keyof typeof sizes;
+
+type Color = keyof typeof colors;
+
+interface Props {
+  className?: string;
+  size?: Size;
+  color?: Color;
+}
+
+const sizes = {
+  sm: 'h-5 w-5',
+  md: 'h-10 w-10',
+  lg: 'h-16 w-16',
+  xl: 'h-24 w-24',
+};
+
+const colors = {
+  primary: 'text-slate-950',
+  white: 'text-white',
+  inherit: '',
+};
+
+export const Spinner: FC<Props> = ({
+  className,
+  size = 'md',
+  color = 'primary',
+}) => (
+  <svg
+    className={cn(sizes[size], colors[color], className)}
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g className="spinner_Wezc" fill="currentColor">
+      <circle cx="12" cy="2.5" r="1.5" stroke="currentColor" opacity=".14" />
+      <circle
+        cx="16.75"
+        cy="3.77"
+        r="1.5"
+        stroke="currentColor"
+        opacity=".29"
+        style={{ background: 'green' }}
+      />
+      <circle
+        cx="20.23"
+        cy="7.25"
+        r="1.5"
+        stroke="currentColor"
+        opacity=".43"
+      />
+      <circle
+        cx="21.50"
+        cy="12.00"
+        r="1.5"
+        stroke="currentColor"
+        opacity=".57"
+      />
+      <circle
+        cx="20.23"
+        cy="16.75"
+        r="1.5"
+        stroke="currentColor"
+        opacity=".71"
+      />
+      <circle
+        cx="16.75"
+        cy="20.23"
+        r="1.5"
+        stroke="currentColor"
+        opacity=".86"
+      />
+      <circle cx="12" cy="21.5" r="1.5" />
+    </g>
+  </svg>
+);
