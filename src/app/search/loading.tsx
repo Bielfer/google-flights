96851134216d324e686041740996a25cfb888a81
Gabriel Fernandes <bielfer18@gmail.com>
2025-01-23 22:13:57 -0300

    fix: improve loading state

--- src/app/search/loading.tsx
+++ src/app/search/loading.tsx
@@ -1,7 +1,21 @@
-import { Spinner } from '~/components/core/spinner';
+import { Skeleton } from '~/components/ui/skeleton';
 
 const Loading = () => {
-  return <Spinner className="mx-auto mt-16" size="lg" />;
+  return (
+    <main className="min-h-screen bg-slate-50">
+      <div className="mx-auto max-w-2xl px-4 py-16 md:px-6 lg:px-8">
+        <Skeleton className="mb-10 h-10" />
+
+        <div className="space-y-6">
+          <Skeleton className="h-64 sm:h-40" />
+          <Skeleton className="h-64 sm:h-40" />
+          <Skeleton className="h-64 sm:h-40" />
+          <Skeleton className="h-64 sm:h-40" />
+          <Skeleton className="h-64 sm:h-40" />
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Loading;
